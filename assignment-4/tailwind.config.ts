d3f6cd2954a87456c6651d5936b9fc170d54e42e
viet <vietvodoi1232@gmail.com>
2023-10-02 21:49:27 +0700

    commit

--- assignment-4/tailwind.config.ts
+++ assignment-4/tailwind.config.ts
@@ -13,6 +13,41 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#e64980',
+        text: '#343a40',
+        bgColor: '#adb5bd',
+        border: '#adb5bd',
+      },
+      padding: {
+        p5: '5px',
+        p10: '10px',
+        p20: '20px',
+        p30: '30px',
+        p50: '50px',
+      },
+      gap: {
+        g10: '10px',
+        g20: '20px',
+      },
+      margin: {
+        m10: '10px',
+        m20: '20px',
+        m30: '30px',
+        m40: '40px',
+        m50: '50px',
+        m80: '80px',
+        
+      },
+      animation: {
+        fadeInOut: 'fadeIn 0.5s ease-in',
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: '0', transform: 'scale(0)' },
+          to: { opacity: '1', transform: 'scale(1)' },
+        },
+      },
     },
   },
   plugins: [],
