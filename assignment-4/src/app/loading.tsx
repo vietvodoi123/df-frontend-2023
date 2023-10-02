a970cbbc5f84f33427ff70b749b1f0f76910a951
viet <vietvodoi1232@gmail.com>
2023-10-02 22:53:37 +0700

    fix loading

--- assignment-4/src/app/loading.tsx
+++ assignment-4/src/app/loading.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function loading() {
   return (
     <div className="loading w-[100px] h-[100px] bg-transparent flex justify-center items-center mt-[130px]">
-      <div className="border-solid border-[5px] border-primary rounded-full border-l-transparent animate-spin text-transparent">
+      <div className="border-solid border-[5px] border-primary rounded-full border-l-transparent animate-spin text-transparent w-full h-full">
         loading
       </div>
     </div>
