4f8d9b20d6e5a905f91e3aef1584ab5fff78e5b6
viet <vietvodoi1232@gmail.com>
2023-10-17 09:38:01 +0700

    change bg

--- assignment-6/src/app/home/layout.tsx
+++ assignment-6/src/app/home/layout.tsx
@@ -5,11 +5,11 @@ import Header from '../component/Header'
 
 function layout(props: { main: React.ReactNode; modal: React.ReactNode }) {
   return (
-    <>
+    <div className=" bg-bgColor dark:bg-bgColorDark w-screen h-screen">
       <Header />
       {props.main}
       {props.modal}
-    </>
+    </div>
   )
 }
 
